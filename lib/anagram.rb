a2e3b837862ad987e063e11655d2e80370d7e35e
Andrei Pestenariu <pclandrei@gmail.com>
2019-12-31 03:47:49 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,7 @@ class Anagram
   
   
   def match(array) 
-    array.any? {|e| e.split('').sort == @word.split('').sort ? true : false }
+    array.any? {|e| e.split('').sort == @word.split('').sort ? e : nil }
   end
   
 end 
